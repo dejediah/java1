b3ef46e8a517981cde5613beb9c8443a47748e9b
GoodIdeasAreOverrated <32150584+GoodIdeasAreOverrated@users.noreply.github.com>
2023-06-07 21:06:39 +0800

    User Input Products
    - Changed pre-defined array into array with max size 64
    - This means we can only add up to 64 products to the array
    - Program now prompts user to add products
    
    - Changed all instances of arr.length into variable n, which matches how many products have been added to the array (A.K.A. effectively how much elements are filled in the array)
        - Added parameter "length" (which reads variable n) to all Functions dependent on arr.length

--- slay.java
+++ slay.java
@@ -10,75 +10,114 @@ package com.mycompany.asdfkljalksdfj;
  */
 public class slay {
     //bubble sort algorithm
-    static void bubbleSort(int[] arr, String[] arr2) {  
-        int n = arr.length;  
+    static void bubbleSort(int[] arr, String[] arr2, int length) {
+        int n = length;
         int temp = 0;
-         for(int i=0; i < n; i++){  
-                 for(int j=1; j < (n-i); j++){  
-                          if(arr[j-1] > arr[j]){  
-                                 //swaps string elements whenever a comparison is made
-                                 String temp2 = arr2[j-1];
-                                 arr2[j-1] = arr2[j];
-                                 arr2[j] = temp2;
-                                 //swaps integer elements whenever a comparison is made
-                                 temp = arr[j-1];
-                                 arr[j-1] = arr[j];  
-                                 arr[j] = temp;  
-                                 
-                         }  
-                          
-                 }  
-         }
-    }
+        for(int i=0; i < n; i++){
+            for(int j=1; j < (n-i); j++){
+                if(arr[j-1] > arr[j]){
+                    //swaps string elements whenever a comparison is made
+                    String temp2 = arr2[j-1];
+                    arr2[j-1] = arr2[j];
+                    arr2[j] = temp2;
+                    //swaps integer elements whenever a comparison is made
+                    temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
 
-        public static int StringSearch(String []arr,String target){
-        int n = arr.length;
-        for(int i = 0; i<n;i++){
-            if(arr[i].equalsIgnoreCase(target) ){
-                return i;
+                }
 
             }
+        }
+    }
 
-
+    public static int stringSearch(String []arr,String target, int length) {
+        int n = length;
+        for(int i = 0; i < n; i++) {
+            if(arr[i].equalsIgnoreCase(target) ){
+                return i;
+            }
         }
         return -1;
-
     }
-    
+
     public static void main(String[] args) {
-        //use *INT* arr[] for the product amount input
-        int arr[] ={3,60,35,2,45,320,5};  
-        //use *STRING* arr2[] for the product name input
-        String arr2[] = {"apple", "orange", "banana", "lotion", "hairspray", "crackers", "oil"};
-                 
-                System.out.println("Unsorted Products");  
-                for(int i=0; i < arr.length; i++){  
-                        System.out.print("Product name: " + arr2[i] + " | " + "Product amount: "+ arr[i] + "\n"); 
-                        System.out.println("-----------"); 
-                }  
-                System.out.println();  
-                  
-                bubbleSort(arr, arr2);//sorting array elements using bubble sort
-                
-                //add user input for ASCENDING, DESCENDING, and SEARCH
-                /* 
+        //array size of 64 means we can only add up to 64 products to array
+            //use *INT* arr[] for the product amount input
+        int arr[] = new int[64];
+            //use *STRING* arr2[] for the product name input
+        String arr2[] = new String[64];
+
+        // n is length of array
+        // increments for each product added to array
+        int n = 0, addRun = 1;
+
+        Scanner am = new Scanner(System.in);
+        Scanner prod = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+
+        while (addRun == 1) {
+            System.out.print("Enter product: ");
+            arr2[n] = prod.nextLine();
+
+            System.out.print("Enter amount: ");
+            arr[n] = am.nextInt();
+
+            n++;
+
+            System.out.println("Do you want to add another product?\n" +
+                    "1. Yes.\n" +
+                    "2. No.");
+            addRun = sc.nextInt();
+
+            while (!(addRun == 1) && !(addRun == 2)) {
+                switch (addRun) {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    default:
+                        System.out.println("Please input a valid option!");
+                        System.out.println("Do you want to add another product?\n" +
+                                "1. Yes.\n" +
+                                "2. No.");
+                        addRun = sc.nextInt();
+                }
+            }
+
+        }
+
+        //add user input for ASCENDING, DESCENDING, and SEARCH
+                /*
                 Menu should look like:
                 Hello! What would you like to do with the inventory?
                 1. Search for specific product.
                 2. Sort inventory in ascending order.
                 3. Sort inventory in descending order.
                 */
-                 
-                System.out.println("Sorted Products (Ascending)");  
-                for(int i=0; i < arr.length; i++){  
-                        System.out.println("Product name: " + arr2[i] + " " + "\nProduct amount: "+ arr[i]); 
-                        System.out.println(" "); 
-                }  
-                System.out.println("Sorted Products (Descending)");  
-                for(int i= (arr.length - 1); i > 0; i--){  
-                        System.out.println("Product name: " + arr2[i] + " " + "\nProduct amount: "+ arr[i]); 
-                        System.out.println(" "); 
-                }
-   
-        }  
+
+
+        System.out.println(" ");
+
+        System.out.println("Unsorted Products");
+        for(int i=0; i < n; i++){
+            System.out.print("Product name: " + arr2[i] + " | " + "Product amount: "+ arr[i] + "\n");
+            System.out.println("-----------");
+        }
+        System.out.println();
+
+        bubbleSort(arr, arr2, n);//sorting array elements using bubble sort
+
+        System.out.println("Sorted Products (Ascending)");
+        for(int i=0; i < n; i++){
+            System.out.println("Product name: " + arr2[i] + " " + "\nProduct amount: "+ arr[i]);
+            System.out.println(" ");
+        }
+        System.out.println("Sorted Products (Descending)");
+        for(int i = (n - 1); i > -1; i--){
+            System.out.println("Product name: " + arr2[i] + " " + "\nProduct amount: "+ arr[i]);
+            System.out.println(" ");
+        }
+
     }
+}
